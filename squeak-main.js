8f839035ff0a5f17b84ed9e46e4148243c0d8c08
adamc295 <mstowe584923@gmail.com>
2016-06-28 19:58:41 +0100

    Creation of project
    PLS NO HATE

---
+++ squeak-main.js
@@ -0,0 +1,19 @@
+/*
+    squeak-main.js
+    This is the main core of Squeak.js (if you could guess by the name)
+    
+    defs:
+    World()
+    MorphTest()
+*/
+
+var World;
+var MorphTest
+
+function World() {
+    //TODO: Create WorldMorph Creation Function
+};
+
+function MorphTest() {
+    //TODO: Create MorphTest Function
+};
